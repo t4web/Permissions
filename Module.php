cb4b4aaa5f0e6f3ab9a0b30a0debf427fa16a47e
maxgu <maxgu@t4web.com.ua>
2015-04-04 15:15:09 +0300

    initial commit

---
+++ Module.php
@@ -0,0 +1,39 @@
+<?php
+namespace T4webPermissions;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ControllerProviderInterface;
+use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\Mvc\Controller\ControllerManager;
+use Zend\Console\Adapter\AdapterInterface as ConsoleAdapterInterface;
+use Zend\ServiceManager\ServiceManager;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface,
+                        ControllerProviderInterface
+{
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getControllerConfig()
+    {
+        return array(
+            'factories' => array(
+            )
+        );
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,63 @@
+<?php
+
+return array(
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+        'display_exceptions' => true,
+        'display_not_found_reason' => true,
+    ),
+
+    'router' => array(
+        'routes' => array(
+            'permissions-list' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route'    => '/permissions',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'T4webPermissions\Controller\Admin',
+                        'controller'    => 'List',
+                        'action'        => 'default',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'console' => array(
+        'router' => array(
+            'routes' => array(
+                'permissions-init' => array(
+                    'options' => array(
+                        'route'    => 'permissions init',
+                        'defaults' => array(
+                            '__NAMESPACE__' => 'T4webPermissions\Controller\Console',
+                            'controller' => 'Init',
+                            'action'     => 'run'
+                        )
+                    )
+                ),
+            )
+        )
+    ),
+
+    'db' => array(
+        'tables' => array(
+            't4webpermissions-roles' => array(
+                'name' => 'roles',
+                'columnsAsAttributesMap' => array(
+                    'id' => 'id',
+                    'name' => 'name',
+                ),
+            ),
+        ),
+    ),
+
+    'criteries' => array(
+        'Role' => array(
+            'empty' => array('table' => 'roles'),
+        ),
+    ),
+);
